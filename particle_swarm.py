2f21664975772a80e391d257e4cc678c0165cb61
Louis Groux <lgroux@nuc2.lrde.epita.fr>
2015-04-16 12:02:16 +0200

    Creates SFML basic example

---
+++ particle_swarm.py
@@ -0,0 +1,17 @@
+import sfml as sf
+
+
+# create the main window
+window = sf.RenderWindow(sf.VideoMode(640, 480), "pySFML Window")
+
+
+# start the game loop
+while window.is_open:
+       # process events
+    for event in window.events:
+              # close window: exit
+        if type(event) is sf.CloseEvent:
+                     window.close()
+
+    window.clear() # clear screen
+    window.display() # update the window
